896e2d0fb1e5a416c67bb1084bf6808e3b86cf9d
Shanara Watts <watts_shan2637@live.com>
2021-12-03 00:44:43 -0500

    Update

--- user_login.php
+++ user_login.php
@@ -1,4 +1,40 @@
 <?php
 
-session_start();
-$host
+require_once 'config.php';
+
+function userLogin($email,$password){
+    if(!(checkLogin($email,$password))){
+        echo "Incorrect email or password";
+        return false;
+    }
+}
+
+function checkLogin($email,$password){
+    $conn = new PDO('mysql:host=localhost; dbname = bugme;', 'root', 'password');
+    $queryLogin = "SELECT 'id', 'firstname', 'lastname' FROM 'Users' WHERE 'email' = '$email'";
+    $stmt = $conn->query($queryLogin);
+    $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($res){
+        $_SESSION["login_id"] = $res['id'];
+        $_SESSION["firstname"] = $res['firstname'];
+        $_SESSION["lastname"] = $res['lastname'];
+        header("Location: ../header.php");
+    }
+
+    else{
+        return false;
+    }
+
+    if(isset($_SESSION["login_id"])){
+        header("Location: ../header.php");
+    }
+
+    if(isset($_POST['login'])){
+        $username = $_POST['email'];
+        $password = $_POST['password'];
+
+        userLogin($email,$password);
+    }
+}
+?>
