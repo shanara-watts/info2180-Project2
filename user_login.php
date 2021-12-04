640e7c96c85307fb33a132b61ef1585aaa2a2ce0
Shanara Watts <watts_shan2637@live.com>
2021-12-03 23:00:42 -0500

    ...

--- user_login.php
+++ user_login.php
@@ -6,7 +6,7 @@ require_once 'config.php';
 
 try {
 
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8mb4", $dbUser, $dbPass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   } catch(PDOException $e) {
